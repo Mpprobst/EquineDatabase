ddca92a8637da9b81a724f0c5fb75ea36af72f9c
Powers, Darren <dcpowe4@uky.edu>
2019-12-03 16:09:09 -0500

    Fixed logout.php's cookie deletion issue

--- functions/php/logout.php
+++ functions/php/logout.php
@@ -1,5 +1,5 @@
 <?php
 require("../../assets/php/redirect_helper.php");
-setcookie("equine_database", "", time()-3600);
+setcookie("equine_database", "", time()-3600, "/");
 header("Location: http://".$ip."/equine/");
 ?>
