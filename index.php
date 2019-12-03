c5434a7d7acaa175f47becfd8b230c3743b22ae1
Michael Probst <mpprobst@gmail.com>
2019-12-03 17:08:29 -0500

    Leg database entry attempt - NewAssessment.php

--- index.php
+++ index.php
@@ -16,7 +16,7 @@ if(isset($_COOKIE["equine_database"])) {
 <div class="container">
 		<div class="input-form" id="Login">
                         <h3>Login:</h3>
-                        <form class="input-form-bordered" action="registration.php" method="post">
+                        <form class="input-form-bordered" action="functions/php/registration.php" method="post">
                                 Username: <input type="text" name="username"><br><br>
                                 Password: <input type="text" name="password"><br><br>
                                 <button type="submit">Submit</button>                                
