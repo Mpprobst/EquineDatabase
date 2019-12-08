eec543a8502ce5743fc2f9c231451a712c3b18b7
Michael Probst <mpprobst@gmail.com>
2019-12-08 12:09:06 -0500

    edited reporting.php

--- equine/analytics/reporting.php
+++ equine/analytics/reporting.php
@@ -1,2 +1,11 @@
-<canvas id="chart" style="max-width: 500px;"></canvas>
-
+<head>
+<!-- Plotly.js -->
+        <script src="../../assets/js/histogram.js"></script>
+        </head>
+        <body>
+        <!-- Plotly chart will be drawn inside this DIV -->
+        <div id="myDiv"></div>
+        <script>
+        /* JAVASCRIPT CODE GOES HERE */
+        </script>
+        </body>
