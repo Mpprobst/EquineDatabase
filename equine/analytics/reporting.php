3ffcc13c8bbbeb80f1a2f8ea81f8223b2ecd8d07
Michael Probst <mpprobst@gmail.com>
2019-12-08 12:33:31 -0500

    updated the readme

--- equine/analytics/reporting.php
+++ equine/analytics/reporting.php
@@ -1,11 +1,26 @@
+<?php
+/* This script uses an open source poltting library, Plotly, to create a D3.js based histogram.   
+ */
+?>
+
 <head>
 <!-- Plotly.js -->
-        <script src="../../assets/js/histogram.js"></script>
+        <script src="https://cdn.plot.ly/plotly-latest.min.js"></script>
         </head>
         <body>
         <!-- Plotly chart will be drawn inside this DIV -->
         <div id="myDiv"></div>
         <script>
-        /* JAVASCRIPT CODE GOES HERE */
+	var x = [];
+	for (var i = 0; i < 500; i ++) {
+	    x[i] = Math.random();
+	}
+
+	var trace = {
+	    x: x,
+	    type: 'histogram',
+	  };
+	var data = [trace];
+	Plotly.newPlot('myDiv', data, {}, {showSendToCloud: true});
         </script>
         </body>
