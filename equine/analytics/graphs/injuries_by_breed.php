0049b839e2642b6598f865de3eb3b93cd79ee632
Darren-Powers <dcpowe4@uky.edu>
2019-12-09 19:04:41 -0500

    Bootstrap and navigation buttons for analytics

--- equine/analytics/graphs/injuries_by_breed.php
+++ equine/analytics/graphs/injuries_by_breed.php
@@ -3,11 +3,11 @@
  */
 
 	require("../../assets/php/mysql_connector.php");
-        require("../../assets/php/redirect_helper.php");
-        $mysqli = new mysqli($host, $SQLuserName, $Pass, $DB);
+	require("../../assets/php/redirect_helper.php");
+	$mysqli = new mysqli($host, $SQLuserName, $Pass, $DB);
 
-        if ($mysqli->connect_error) {
-                die("Connection failed: " . $mysqli->connect_error);
+	if ($mysqli->connect_error) {
+		die("Connection failed: " . $mysqli->connect_error);
 	}
 	$bad_query = "SELECT COUNT(sid) AS InjuryCount, Horse.Hbreed FROM CasePathology, Assessment, Horse WHERE CasePathology.Cid = Assessment.Cid AND Assessment.Chorse = Horse.Hid AND CasePathology.Pid > 2 GROUP BY Horse.Hbreed;";
 	$count_query = "SELECT COUNT(sid) AS InjuryCount, Horse.Hbreed FROM CasePathology, Assessment, Horse WHERE CasePathology.Cid = Assessment.Cid AND Assessment.Chorse = Horse.Hid GROUP BY Horse.Hbreed;";
@@ -31,34 +31,69 @@
 		array_push($bad_array, $row["InjuryCount"]);
 	}
 ?>
-<!-- Plotly.js -->
-        <script src="https://cdn.plot.ly/plotly-latest.min.js"></script>
-        </head>
-        <body>
-        <!-- Plotly chart will be drawn inside this DIV -->
-        <div id="myDiv"></div>
-        <script type="text/javascript">
 
-        var breeds = <?php echo json_encode($breed_array); ?>;
-        var percents = [];
-        var total_counts = <?php echo json_encode($count_array); ?>;
-        var bad_counts = <?php echo json_encode($bad_array); ?>;
-        for (var i = 0; i < bad_counts.length; i++) {
-                percents[i] = 100 * bad_counts[i] / total_counts[i];
-        }
-        var trace = {
-                x: breeds,
-                y: percents,
-                type: 'bar',
-          };
-	var data = [trace];
-	var layout = {
-		title: "Percentage of Assessments that are Injuries By Breed",
-		xaxis: {title: "Breed"},
-		yaxis: {title: "Percentage (%)"}
+<!doctype html>
+<html lang="en">
+<head>
+<title>Equine Project | TES Report</title>
+	<meta charset="UTF-8" />
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<link href="../../assets/bootstrap-4.3.1-dist/css/bootstrap.css" type="text/css" rel="stylesheet">
+	<link href="../../assets/css/style.css" type="text/css" rel="stylesheet" />
+	<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
+	<script src="../../assets/bootstrap-4.3.1-dist/js/bootstrap.min.js"></script>
+	
+	<style>
+		.GraphArea {
+			width: 100%;
+			height: 20rem;
+		}
+	</style>
+
+</head>
+<body>
+<div class="container">
+	<div class="row">
+		<div class="col-sm-12">
+			<h2>Injury Types By Breed Report</h2>
+			<!-- Plotly chart will be drawn inside this DIV -->
+			<div class="row">
+				<div class="col-sm-3"></div>
+				<div class="col-sm-6">
+					<div id="Chart" class="GraphArea"></div>
+					<div class="btn-group">
+						<a href="tes.php" class="btn btn-primary mr-2">TES Report</a>
+						<a href="../reporting.php" class="btn btn-primary mr-2">Reporting Home</a>
+						<a href="../../home.php" class="btn btn-secondary">Home</a>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+</div>
+<script src="https://cdn.plot.ly/plotly-latest.min.js"></script><!-- Plotly.js -->
+<script type="text/javascript">
+
+var breeds = <?php echo json_encode($breed_array); ?>;
+var percents = [];
+var total_counts = <?php echo json_encode($count_array); ?>;
+var bad_counts = <?php echo json_encode($bad_array); ?>;
+for (var i = 0; i < bad_counts.length; i++) {
+	percents[i] = 100 * bad_counts[i] / total_counts[i];
+}
+var trace = {
+	x: breeds,
+	y: percents,
+	type: 'bar',
 	};
-        Plotly.newPlot('myDiv', data, layout, {showSendToCloud: true});
-        </script>
-        </body>
+var data = [trace];
+var layout = {
+	title: "Percentage of Assessments that are Injuries By Breed",
+	xaxis: {title: "Breed"},
+	yaxis: {title: "Percentage (%)"}
+};
+Plotly.newPlot('Chart', data, layout, {showSendToCloud: true});
+</script>
+</body>
 
 
--- equine/analytics/graphs/tes.php
+++ equine/analytics/graphs/tes.php
@@ -3,11 +3,11 @@
  */
 
 	require("../assets/php/mysql_connector.php");
-        require("../assets/php/redirect_helper.php");
-        $mysqli = new mysqli($host, $SQLuserName, $Pass, $DB);
+	require("../assets/php/redirect_helper.php");
+	$mysqli = new mysqli($host, $SQLuserName, $Pass, $DB);
 
-        if ($mysqli->connect_error) {
-                die("Connection failed: " . $mysqli->connect_error);
+	if ($mysqli->connect_error) {
+			die("Connection failed: " . $mysqli->connect_error);
 	}
 	$query = "SELECT COUNT(sid) AS InjuryCount, Horse.Hbreed FROM CasePathology, Assessment, Horse WHERE CasePathology.Cid = Assessment.Cid AND Assessment.Chorse = Horse.Hid AND CasePathology.Pid > 2 GROUP BY Horse.Hbreed;";
 	$result = $mysqli->query($query);
@@ -20,14 +20,47 @@
 		//echo "<br>breed=" . $result_array[0] . "<br>count=" . $row["InjuryCount"] . "<br>";
 	}
 ?>
+<!doctype html>
+<html lang="en">
 <head>
-<!-- Plotly.js -->
-        <script src="https://cdn.plot.ly/plotly-latest.min.js"></script>
-        </head>
-        <body>
-        <!-- Plotly chart will be drawn inside this DIV -->
-        <div id="myDiv"></div>
-	<script type="text/javascript">
+<title>Equine Project | TES Report</title>
+	<meta charset="UTF-8" />
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<link href="../../assets/bootstrap-4.3.1-dist/css/bootstrap.css" type="text/css" rel="stylesheet">
+	<link href="../../assets/css/style.css" type="text/css" rel="stylesheet" />
+	<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
+    <script src="../../assets/bootstrap-4.3.1-dist/js/bootstrap.min.js"></script>
+	
+	<style>
+		.GraphArea {
+			width: 100%;
+			height: 20rem;
+		}
+	</style>
+
+</head>
+<body>
+<div class="container">
+	<div class="row">
+		<div class="col-sm-12">
+			<h2>TES Report</h2>
+			<!-- Plotly chart will be drawn inside this DIV -->
+			<div class="row">
+				<div class="col-sm-3"></div>
+				<div class="col-sm-6">
+					<div id="Chart" class="GraphArea"></div>
+					<div class="btn-group">
+						<a href="injuries_by_breed.php" class="btn btn-primary mr-2">Injuries by Breed</a>
+						<a href="../reporting.php" class="btn btn-primary mr-2">Reporting Home</a>
+						<a href="../../home.php" class="btn btn-secondary">Home</a>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+</div>
+<script src="https://cdn.plot.ly/plotly-latest.min.js"></script><!-- Plotly.js -->
+<script type="text/javascript">
 
 	var breeds = <?php echo json_encode($breed_array); ?>;
 	var counts = <?php echo json_encode($count_array); ?>;
@@ -38,7 +71,8 @@
 		type: 'bar',
 	  };
 	var data = [trace];
-	Plotly.newPlot('myDiv', data, {}, {showSendToCloud: true});
-	</script>
-        </body>
+	Plotly.newPlot('Chart', data, {}, {showSendToCloud: true});
+</script>
+</body>
 
+</html>
